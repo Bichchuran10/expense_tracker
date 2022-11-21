5ac27178e7496a844c7816c6a21797568cb95e39
Bichchuran10 <bichchuran10@gmail.com>
2022-11-22 02:05:46 +0530

    working properly

---
+++ index.js
@@ -0,0 +1,65 @@
+function saveToLocalStorage(event)
+{
+  event.preventDefault()
+    let expenseAmount=event.target.amount.value
+    let description=event.target.description.value
+    let type=event.target.category.value
+
+    let expense={
+        expenseAmount,
+        description,
+        type
+    }
+
+    //adding to the local storage
+    localStorage.setItem(expense.expenseAmount,JSON.stringify(expense))
+    showNewExpenseOnScreen(expense)
+}
+window.addEventListener("DOMContentLoaded", () => {
+    const localStorageObj = localStorage;
+    const localstoragekeys  = Object.keys(localStorageObj)
+
+    for(var i =0; i< localstoragekeys.length; i++){
+        const key = localstoragekeys[i]
+        const expensesString = localStorageObj[key];
+        const expenseDetailsObj = JSON.parse(expensesString);
+        showNewExpenseOnScreen(expenseDetailsObj)
+    }
+})
+
+
+
+function showNewExpenseOnScreen(expense)
+{
+    let parentNode=document.getElementById('listOfExpenses')
+    let childHTML=`<li id=${expense.expenseAmount}> ${expense.expenseAmount}- ${expense.description}- ${expense.type}
+                        <button onclick=deleteExpense('${expense.expenseAmount}')> Delete Expense </button>
+                        <button onclick=editExpenseDetails('${expense.expenseAmount}','${expense.description}','${expense.type}')>Edit Expense </button>
+                    </li>`
+                    //console.log(`${expense.expenseAmount},${expense.description},${expense.type}`)
+    parentNode.innerHTML=parentNode.innerHTML+childHTML
+}
+
+//edit
+function editExpenseDetails(expenseAmount,description,type)
+{
+    document.getElementById('amount').value=expenseAmount;
+    document.getElementById('description').value=description;
+    document.getElementById('category').value=type;
+console.log(expenseAmount,description,type)
+deleteExpense(expenseAmount)
+}
+
+//for deletion from localStorage
+function deleteExpense(amount){
+    localStorage.removeItem(amount);
+    removeExpenseFromScreen(amount);
+}
+
+//for deletion from screen
+function removeExpenseFromScreen(amount){
+    const parentNode = document.getElementById('listOfExpenses');
+    const childNodeToBeDeleted = document.getElementById(amount);
+
+    parentNode.removeChild(childNodeToBeDeleted)
+}
